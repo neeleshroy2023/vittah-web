0a2d1c1843d3e58e5291341415aba434dc866431
Neelesh Roy <128451760+neeleshroy2023@users.noreply.github.com>
2023-11-17 16:36:59 +0000

    nextjs setup initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Vittah - Loan origination systems',
+  description: 'Unlocking Financial Potential: Elevate Efficiency, Amplify Growth with [Product Name] – Where Loans Begin, Success Prevails!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
